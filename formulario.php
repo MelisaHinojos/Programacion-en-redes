5b1fef88389ecb2e4e439a054b860f181d87bcc9
MelisaHinojos <90910185+MelisaHinojos@users.noreply.github.com>
2022-11-01 14:43:27 +0100

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -4,14 +4,18 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
+    <div class="header"><br>
+      <a>Formulario Alta</a>
+    </div>
+    
     <form action="procesar.php" method="post">
-        Nombre<input name="nombre" type="text"><br>
-        Apellido<input name="apellido" type="text"><br>
-        Usuario<input name="usuario" type="text"><br>
-        Email<input name="email" type="email"><br>
-        Contraseña<input name="contra" type="password"><br>
-        Foto de Perfil<input name="imagen" type="file"><br>
-        <input type="submit" value="Enviar">
+        Nombre <input name="nombre" type="text"><br>
+        Apellido <input name="apellido" type="text"><br>
+        Usuario <input name="usuario" type="text"><br>
+        Email <input name="email" type="email"><br>
+        Contraseña <input name="contra" type="password"><br>
+        Foto de Perfil <br><br><input name="imagen" type="file"><br>
+        <input type="submit" value="Enviar" class="boton">
     </form>
   </body>
 </html>
