b23ed1c44d51f137f89f9583e2ab963affa4ecb5
agustin gamarra <agustingamarra201@gmail.com>
2022-11-23 21:57:23 -0300

    codigo

---
+++ trabajo final/admin.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="CSS/style.css">
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+    <form method="POST">
+        Codigo admin<input name="nombre" type="text"><br>
+        <input class="boton" type="submit" value="Enviar" name="iniciar">
+    </form>
+   
+  </body>
+</html>
